499e9c88a1cd6ec3f103cb07756126da8856cbf1
Colin Fendrick <colin.fendrick@gmail.com>
2020-11-19 16:56:45 -0800

    Now with ssr

---
+++ src/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+require('svelte/register');
+
+const name = 'Planet';
+
+const template = `
+  <html>
+    <head>
+      HEAD
+      <style>
+        CSS
+      </style>
+      <script type='module'>
+        import App from './build/App.svelte';
+        new App({
+          hydrate: true
+        });
+      </script>
+    </head>
+    <body>
+      HTML
+    </body>
+  </html>
+`;
+
+const app = express();
+//express.static.mime.define({'text/html': ['svelte']});
+app.use(express.static('public'));
+
+app.get('/ssr', (req, res) => {
+  // See https://svelte.dev/docs#Server-side_component_API
+  const App = require('../public/build/ssr.js');
+  const {
+    head,
+    html,
+    css
+  } = App.render({
+    name
+  });
+  console.log('server.js x: html =', html);
+  const result = template
+    .replace('HEAD', head)
+    .replace('CSS', css.code)
+    .replace('HTML', html);
+  res.set('Content-Type', 'text/html');
+  res.end(result);
+});
+
+const PORT = 1919;
+app.listen(PORT, () => console.log('listening on', PORT));
